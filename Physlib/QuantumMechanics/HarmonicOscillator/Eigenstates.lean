ba8de237aa0e3db40bd54413313764dc7733a720
gloges <gerg1995@gmail.com>
2026-08-16 00:19:16 +0900

    eigenfunctions

--- Physlib/QuantumMechanics/HarmonicOscillator/Eigenstates.lean
+++ Physlib/QuantumMechanics/HarmonicOscillator/Eigenstates.lean
@@ -5,10 +5,24 @@ Authors: Gregory J. Loges
 -/
 module
 
+public import Physlib.Mathematics.InnerProductSpace.Gaussian
+public import Physlib.Mathematics.SpecialFunctions.PhysHermite
 public import Physlib.QuantumMechanics.HarmonicOscillator.Basic
 /-!
 
 # Energy eigenstates of the quantum harmonic oscillator
+
+## i. Overview
+
+## ii. Key results
+
+## iii. Table of contents
+
+- A. Energy eigenvalues
+- B. Cartesian basis
+  - B.1. Eigenfunctions
+
+## iv. References
 
 -/
 @[expose] public section
@@ -17,9 +31,10 @@ noncomputable section
 namespace QuantumMechanics
 namespace HarmonicOscillator
 
-open Constants Finset
+open Complex Constants Finset InnerProductSpace Polynomial SchwartzMap Space SpaceDHilbertSpace
+open scoped Nat Real ComplexConjugate
 
-variable {d : ℕ} (Q : HarmonicOscillator d) (n : Fin d → ℕ)
+variable {d : ℕ} (Q : HarmonicOscillator d) (n n' : Fin d → ℕ) (x : Space d)
 
 /-!
 ## A. Energy eigenvalues
@@ -34,6 +49,56 @@ lemma eigenEnergy_strictMono : StrictMono Q.eigenEnergy := by
   obtain ⟨h, i, hi⟩ := Pi.lt_def.mp h
   exact sum_lt_sum (fun i _ ↦ by simp [h i]) ⟨i, mem_univ i, by simp [hi]⟩
 
+/-!
+## B. Cartesian basis
+-/
+
+/-!
+### B.1. Eigenfunctions
+-/
+
+@[fun_prop]
+lemma _root_.Function.HasTemperateGrowth.prod {ι : Type*} [DecidableEq ι] {s : Finset ι}
+    {E F : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [NormedCommRing F] [NormedAlgebra ℝ F]
+    {f : ι → E → F} (hf : ∀ i ∈ s, Function.HasTemperateGrowth (f i)) :
+    Function.HasTemperateGrowth (∏ i ∈ s, f i) := by
+  induction s using induction_on' with
+  | empty => exact .const _
+  | insert j t hjs hts hjt ih =>
+    simp_rw [insert_eq, prod_union (disjoint_singleton_left.mpr hjt), prod_singleton]
+    exact .fun_mul (hf j <| mem_insert_self j t) (ih fun i h ↦ hf i <| mem_insert_of_mem h)
+
+@[fun_prop]
+lemma _root_.Function.HasTemperateGrowth.fun_prod {ι : Type*} [DecidableEq ι] {s : Finset ι}
+    {E F : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [NormedCommRing F] [NormedAlgebra ℝ F]
+    {f : ι → E → F} (hf : ∀ i ∈ s, Function.HasTemperateGrowth (f i)) :
+    Function.HasTemperateGrowth (fun x ↦ ∏ i ∈ s, f i x) :=
+  prod_fn s f ▸ Function.HasTemperateGrowth.prod hf
+
+@[fun_prop]
+lemma _root_.Space.val_hasTemperateGrowth (i : Fin d) :
+    Function.HasTemperateGrowth (Space.val · i) := by
+  simp_rw [← basis_inner]
+  exact Function.hasTemperateGrowth_inner_right _
+
+def coeff (i : Fin d) : ℝ := 1 / √(2 ^ n i * (n i)! * √π * √(Q.ξ i))
+
+lemma coeff_eq (i : Fin d) : Q.coeff n i = 1 / √(2 ^ n i * (n i)! * √π * √(Q.ξ i)) := rfl
+
+def eigenfunction : 𝓢(Space d, ℂ) :=
+  compCLMOfContinuousLinearEquiv ℂ Q.ξEquiv.symm <|
+    smulLeftCLM ℂ (fun x ↦ ∏ i, Q.coeff n i * physHermite (n i) (x i)) (stdGaussian (Space d) ℂ)
+
+lemma eigenfunction_eq :
+    Q.eigenfunction n = compCLMOfContinuousLinearEquiv ℂ Q.ξEquiv.symm (smulLeftCLM ℂ
+      (fun x ↦ ∏ i, Q.coeff n i * physHermite (n i) (x i)) (stdGaussian (Space d) ℂ)) := rfl
+
+lemma eigenfunction_apply :
+    Q.eigenfunction n x =
+      ∏ i, Q.coeff n i * physHermite (n i) (x i / Q.ξ i) * cexp (-2⁻¹ * (x i / Q.ξ i) ^ 2) := by
+  rw [eigenfunction_eq, compCLMOfContinuousLinearEquiv_apply, Function.comp_apply,
+    smulLeftCLM_apply_apply (by fun_prop), ξEquiv_symm_apply]
+  simp [div_eq_mul_inv, prod_mul_distrib, exp_neg, norm_sq_eq, mul_sum, mul_comm, exp_sum]
 end HarmonicOscillator
 end QuantumMechanics
 end
